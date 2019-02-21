b33093abb89dbe360c1c4eb3f445962c55833660
Kane Ryans <kaneryans@hotmail.co.uk>
2019-02-21 18:26:54 +0800

    added and refined routes - teacher validated

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :orders, only: [ :index, :show, :create, :edit, :update ] do
+    resources :messages, only: [ :index, :create ]
+    resources :reviews, only: [ :create ]
+  end
+
+  resources :buyer_profiles
+  resources :dressmaker_profiles
+
+  resources :favourites, only: [ :create, :destroy ]
 end
