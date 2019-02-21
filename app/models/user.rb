a0a700571fe83a56d641e0f8052ef42adc59d65f
John Higgins <johnhiggins@outlook.ie>
2019-02-21 16:07:46 +0800

    added associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :buyer_profile
+  has_one :dressmaker_profile
+  has_many :messages
 end
