6d721e88d573346a37d9046928e85de9f066566a
Kane Ryans <kaneryans@hotmail.co.uk>
2019-02-22 14:02:21 +0800

    added redirect after sign up

--- app/controllers/buyer_profiles_controller.rb
+++ app/controllers/buyer_profiles_controller.rb
@@ -1,11 +1,15 @@
 class BuyerProfilesController < ApplicationController
 
   def show
+    @user = current_user
   end
 
   def edit
+
   end
 
   def update
+    @user = current_user
+    @user.update
   end
 end
