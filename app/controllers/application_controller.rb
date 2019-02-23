d3612fbc8132f03c3fca70b14578a0d3ebfc404e
Kane Ryans <kaneryans@hotmail.co.uk>
2019-02-23 08:38:09 +0800

    removed old code from application controller which was moved to registrations controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
@@ -13,28 +12,7 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
-  protected
-
-  def after_sign_up_path_for(resource)
-    if resource.dressmaker
-      new_dressmaker = DressmakerProfile.new
-      new_dressmaker.user = current_user
-      # raise
-      if new_dressmaker.save
-        dressmaker_profile_path(new_dressmaker)
-      else
-        render 'new_user_registration'
-      end
-    else
-      buyer_profile_path(current_user)
-    end
-  end
-
   private
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :dressmaker])
-  end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
