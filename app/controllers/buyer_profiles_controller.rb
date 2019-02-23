986e769b6f40c6e64d06da044c134d5de510f1af
John Higgins <johnhiggins@outlook.ie>
2019-02-23 10:05:26 +0800

    added link to new buyer profile

--- app/controllers/buyer_profiles_controller.rb
+++ app/controllers/buyer_profiles_controller.rb
@@ -9,4 +9,29 @@ class BuyerProfilesController < ApplicationController
     @user.update
     authorize @user
   end
+
+  def new
+    @buyer = BuyerProfile.new
+    authorize @buyer
+  end
+
+  def create
+    @buyer = BuyerProfile.new#(buyer_params)
+
+    if @buyer.save
+      redirect_to buyer_profile_path(new_buyer)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def buyer_params
+    params.require(:buyer).permit(:size, :style)
+  end
+
+  def set_buyer
+    @buyer = current_user
+  end
 end
