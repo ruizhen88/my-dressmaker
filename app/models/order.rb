cb184408591a8affa706e3f6b5fc52a815a4c833
Yejin Jung <yejiin.jung@gmail.com>
2019-02-25 16:04:09 +0800

    added dependent:destroy to every belongs_to and created new order form view

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
-  belongs_to :user
-  belongs_to :dressmaker, class_name: "User"
+  belongs_to :user, dependent: :destroy
+  belongs_to :dressmaker, class_name: "User", dependent: :destroy
 
   validates :cost, :completion_date, :quantity, :order_details, :status, presence: true
   validates_numericality_of :cost, :quantity
--- app/models/user_speciality.rb
+++ app/models/user_speciality.rb
@@ -1,4 +1,4 @@
 class UserSpeciality < ApplicationRecord
-  belongs_to :dressmaker_profile
-  belongs_to :speciality
+  belongs_to :dressmaker_profile, dependent: :destroy
+  belongs_to :speciality, dependent: :destroy
 end
